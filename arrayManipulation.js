9ed103ece8741cf8d0d279691fee636b6c4bcd4d
Amegbletor Boniface Yaw <byamegbletor@st.ug.edu.gh>
2024-05-18 09:05:16 +0000

    Solution to task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -15,3 +15,26 @@ function processArray(arrNum) {
 }
 
 console.log(processArray([1,2,3,4,5,10,21,30]))
+
+
+// Task 2
+
+function formatArrayStrings(arrStr, number) {
+    let string = []
+
+    for (let i = 0; i < arrStr.length; i++) {
+        let str = arrStr[i]
+        let num = number[i]
+
+        if (num % 2 == 0) {
+            string[string.length] = str.toUpperCase()
+        } else {
+            string[string.length] = str.toLowerCase()
+        }
+    }
+
+    return string
+}
+
+const a = formatArrayStrings(["hi", "hello", "hey", "sup", "App"], processArray([1,2,3,4,5]))
+console.log(a)
